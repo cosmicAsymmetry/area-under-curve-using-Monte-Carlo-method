4b1a7b8e1ca761def0552bb8fff42ff4a82a4f46
cosmicasymmetry <cosmicasymmetry@users.noreply.github.com>
2017-05-23 16:03:29 +0530

    Added Rectangle Method and the function BS...

--- auc.py
+++ auc.py
@@ -1,59 +1,73 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from random import randint
+from random import randrange
 
-lowerbound = input("Enter lower bound: ")
-upperbound = input("Enter upper bound: ")
-randPoints = input("Number of Random Points: ")
-
-xAxis = range(lowerbound, upperbound+1)
-yAxis = []
-sX = 0
-sY = 0
-undercurve = 0 
-upperlimit = 0
-lowerlimit = 0
-n = 0
 def f(x):
     y = x
     return y
-#------
-while n >= lowerbound:
-    if f(n) < lowerlimit:
-        lowerlimit = f(n)
-    yAxis.insert(0, f(n))
-    n-=1
-n = 1  
-#------
-while n <= upperbound:
-    if f(n) > upperlimit:
-        upperlimit = f(n)
-    yAxis.append(f(n))
-    n+=1
-n = 0
 
-while n <= randPoints:
-    sX = randint(lowerbound,upperbound)
-    sY = randint(lowerlimit, upperlimit)
-    if sY <= f(sX) and sY > 0:
-        undercurve+=1
-    if sY >= f(sX) and sY < 0:
-        undercurve+=1
-    plt.plot([sX], [sY], marker='o', markersize=3, color="red")
-    n+=1
-#-----Axis
-x = np.linspace(0.2,10,100)
+def monteCarloMethod(lowerbound, upperbound, randPoints, increment, plotFunction=True):
+    xAxis = np.arange(lowerbound, upperbound+increment, increment).tolist()
+    yAxis = []
+    sX = 0.0
+    sY = 0.0
+    undercurve = 0.0 
+    upperlimit = 0.0
+    lowerlimit = 0.0
+    n = 0.0
+    
+    while n >= lowerbound:
+        if f(n) < lowerlimit:
+            lowerlimit = f(n)
+        yAxis.insert(0, f(n))
+        n-= increment
+    n = increment  
+    
+    while n <= upperbound:
+        if f(n) > upperlimit:
+            upperlimit = f(n)
+        yAxis.append(f(n))
+        n+= increment
+    n = 0
+    
+    while n <= randPoints:
+        sX = randrange(lowerbound*100, upperbound*100+100, increment*100)*0.01
+        sY = randrange(lowerlimit*100, upperlimit*100+100, increment*100)*0.01
+        if sY <= f(sX) and sY > 0:
+            undercurve+=1
+        if sY >= f(sX) and sY < 0:
+            undercurve+=1
+        if plotFunction:
+            plt.plot([sX], [sY], marker='o', markersize=1, color="red")
+        n += increment
+    if plotFunction:
+        # Axis
+        plt.axhline(y=0, color='k')
+        plt.axvline(x=0, color='k')
+        #-----
+        plt.plot(xAxis, yAxis)
+        plt.axis([lowerbound, upperbound, lowerlimit, upperlimit])
+        plt.show()
+    
 
-plt.axhline(y=0, color='k')
-plt.axvline(x=0, color='k')
-#-----
-plt.plot(xAxis, yAxis)
-plt.axis([lowerbound, upperbound, lowerlimit, upperlimit])
+    return((abs(upperlimit) + abs(lowerlimit)) * (abs(upperbound) + abs(lowerbound)) * undercurve / randPoints)
 
-print(undercurve)
-print(abs(upperlimit) + abs(lowerlimit))
-print(abs(upperbound) + abs(lowerbound))
-print(undercurve / randPoints)
-print(randPoints)
-print("Area = "+str((abs(upperlimit) + abs(lowerlimit)) * (abs(upperbound) + abs(lowerbound)) * undercurve / randPoints))
-plt.show()
+def rectangleMethod(lowerbound, upperbound, increment=0.0001, plotFunction=True):
+    area = 0.0
+    while lowerbound < upperbound:
+        area += abs(f(lowerbound)*increment)
+        lowerbound += increment
+    return(area)
+
+monteCarloCount = 0
+rectangleCount = 0
+if __name__ == '__main__':
+    for i in xrange(7):
+        errorRectangle =  abs(rectangleMethod(-10, 10, 1.0/10**(i+1), False) - 100)
+        print("Rectangle: "+str()+str(errorRectangle))
+        errorMonteCarlo = abs( (monteCarloMethod(-10,10, 10**(i+1) ,1,False)) - 100)
+        print("Monte Carlo: "+str(errorMonteCarlo))
+        if errorMonteCarlo < errorRectangle:
+            monteCarloCount += 1
+        else: rectangleCount += 1
+
